b05a49e172608eebe5c970ae94d66a5201b8cad4
jhahn1 <jeremydhahn@gmail.com>
2019-12-02 13:26:12 -0600

    Day 2 Part 1

---
+++ day-02-program-alarm/program-alarm.js
@@ -0,0 +1,30 @@
+const _ = require("lodash");
+
+const programAlarm = input => {
+  const opCodes = input
+    .split(",")
+    .map(x => x.trim())
+    .map(x => parseInt(x));
+
+  for (let i = 0; i < opCodes.length; i += 4) {
+    let code = opCodes[i];
+    let firstIndex = opCodes[i + 1];
+    let secondIndex = opCodes[i + 2];
+    let replaceIndex = opCodes[i + 3];
+    let replacement;
+
+    if (code === 99) {
+      return opCodes;
+    } else if (code === 1) {
+      replacement = opCodes[firstIndex] + opCodes[secondIndex];
+    } else if (code === 2) {
+      replacement = opCodes[firstIndex] * opCodes[secondIndex];
+    } else {
+      console.error("Error: OpsCode = ", code);
+    }
+
+    opCodes[replaceIndex] = replacement;
+  }
+};
+
+module.exports = programAlarm;
