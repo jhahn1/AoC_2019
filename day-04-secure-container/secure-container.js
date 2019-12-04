16c12cb48208111ff62dcf67bf14e5f08fca9e9b
jhahn1 <jeremydhahn@gmail.com>
2019-12-04 14:14:32 -0500

    Part 2 solved

--- day-04-secure-container/secure-container.js
+++ day-04-secure-container/secure-container.js
@@ -1,34 +1,41 @@
 const secureContainer = input => {
-    const rangeList = input.split('-').map(x => x.trim()).map(x => parseInt(x));
-    let possiblePasswordCount = 0;
+  const rangeList = input
+    .split("-")
+    .map(x => x.trim())
+    .map(x => parseInt(x));
+  let possiblePasswordCount = 0;
 
-    for(let i = rangeList[0]; i<=rangeList[1]; i++) {
-        const number = i.toString().split('').map(x => x.trim()).map(x => parseInt(x));
-        if(passesConditions(number)){
-            possiblePasswordCount++;
-        }
+  for (let i = rangeList[0]; i <= rangeList[1]; i++) {
+    if (passesConditions(i)) {
+      possiblePasswordCount++;
     }
-    return possiblePasswordCount;
-}
+  }
+  return possiblePasswordCount;
+};
 
-const passesConditions = digits => {
-    let hasRepeatingDigit = false;
+const passesConditions = number => {
+  const digits = number
+    .toString()
+    .split("")
+    .map(x => x.trim())
+    .map(x => parseInt(x));
+  let hasDoubleDigit = false;
 
-    for(let i = 1; i<digits.length; i++) {
-        if(digits[i]<digits[i-1]){
-            return false;
-        } else {
-            if(digits[i] === digits[i-1]) {
-                hasRepeatingDigit = true;
-            }
-        }
-    }
-
-    if(hasRepeatingDigit) {
-        return true;
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] < digits[i - 1]) {
+      return false;
     } else {
-        return false;
+      if (digits[i] === digits[i + 1] && digits[i] !== digits[i + 2] && digits[i] !== digits[i -1]) {
+          hasDoubleDigit = true;
+      }
     }
-}
+  }
 
-module.exports = secureContainer;
+  if (hasDoubleDigit) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { secureContainer, passesConditions };
